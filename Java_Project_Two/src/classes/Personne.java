9614ab3d04223be66befb9ded305a3f0280d1717
Thibaut <tibeloute@msn.com>
2022-06-12 16:12:13 +0200

    Passage des attributs de Personne en Privé part 1 - ajouts des accesseurs - modification de setnum pour qu'il ne recoivent pas de chiffre en dessous de 1 - modification dans Main pour afficher/modifier les attributs de la class Personne

--- Java_Project_Two/src/classes/Personne.java
+++ Java_Project_Two/src/classes/Personne.java
@@ -2,16 +2,47 @@ package classes;
 
 public class Personne {
 
-	// class public + attribut public
-	 public int num;
-	 public String nom;
-	 public String prenom;
+	// class public + attribut private
+	private int num;
+	private String nom;
+	private String prenom;
+
 	
+
+
+	// constructeur.s accesseurs
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		if (num < 1) {
+			System.out.println("il est interdit de mettre une valeur négative");
+		} else {
+			this.num = num;
+		}
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
 	// construction du toString pour l'affiche de l'objet sysout(Personne1)
-	 @Override
+	@Override
 	public String toString() {
 		return "Personne [num=" + num + ", nom=" + nom + ", prenom=" + prenom + "]";
 	}
-	
-	
+
 }
